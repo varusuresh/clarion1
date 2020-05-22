a2cfef7949af971a445306e6723a4d69ada9983e
varusuresh <65656709+varusuresh@users.noreply.github.com>
2020-05-22 11:11:02 +0530

    assgn1&2
    1&2

---
+++ DictionaryGeneration.java
@@ -0,0 +1,21 @@
+package java8.features;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class DictionaryGeneration {
+	private static Logger logger=Logger.getLogger(Logger.GLOBAL_LOGGER_NAME) ;
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		logger.info("Enter Integral Number:");
+		int num=in.nextInt();
+		List<String> list=new ArrayList<>();
+		for(int i=1;i<=num;i++){
+			list.add(i+":"+i*i);
+		}
+		logger.info("Dictionary is:"+list);
+	}
+
+}
